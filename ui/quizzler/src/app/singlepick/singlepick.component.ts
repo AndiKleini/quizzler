e62846f50e34cdf5b4fd02fb9263ea71227ccefc
aklein <andreaskleinbichler@Andreass-MacBook-Pro.local>
2025-06-02 07:21:14 +0200

    feature US_1: add first version of single pick form

--- ui/quizzler/src/app/singlepick/singlepick.component.ts
+++ ui/quizzler/src/app/singlepick/singlepick.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, ReactiveFormsModule, FormBuilder, FormArray} from '@angular/forms';
 
 @Component({
   selector: 'quizzler-singlepick',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './singlepick.component.html',
   styleUrl: './singlepick.component.css'
 })
-export class SinglepickComponent {
-
+export class SinglepickComponent implements OnInit  {
+  selectedIndex = -1;
+  singlePickForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.singlePickForm = this.formBuilder.group( {
+      options: this.formBuilder.array([])
+    });
+  }
+  ngOnInit(): void {
+    // add for each option a line with a checkbox
+    this.options.push(this.formBuilder.control('0'));
+    this.options.push(this.formBuilder.control('1'));
+    this.options.push(this.formBuilder.control('2'));
+  }
+  get options() {
+    return this.singlePickForm.get('options') as FormArray;
+  }
 }
