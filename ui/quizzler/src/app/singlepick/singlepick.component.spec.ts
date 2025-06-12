1a03ae7852bfa7bc2d244066bb172190044f66c4
AndiKleini <andi.kleinbichler@gmail.com>
2025-06-12 06:56:34 +0200

    feature US_1: improve test coverage of toggle behaviour

--- ui/quizzler/src/app/singlepick/singlepick.component.spec.ts
+++ ui/quizzler/src/app/singlepick/singlepick.component.spec.ts
@@ -10,17 +10,7 @@ describe('SinglepickComponent', () => {
   let fixture: ComponentFixture<SinglepickComponent>;
 
   it ('should display initally all options', async () => {
-    let mockQuestionService = {
-       getSinglePickQuestionById: jest.fn().mockReturnValue( new SinglePickQuestion(
-                   "Question ES 1", 
-                   "This is the text of a single pick question !",
-                   [ 
-                     new SingePickOption(1, 'Option 1'), 
-                     new SingePickOption(2, 'Option 2'),
-                     new SingePickOption(3, 'Option 3'),
-                     new SingePickOption(4, 'Option 4')
-                   ]))
-    };
+    let mockQuestionService = returnUntouchedOptions();
     await TestBed.configureTestingModule({
       imports: [SinglepickComponent],
       providers: [
@@ -37,18 +27,8 @@ describe('SinglepickComponent', () => {
     expect(options).toBeTruthy();
     expect((options as FormArray).length).toEqual(4);
   });
-  it ('should select first option on first click', () => {
-    let mockQuestionService = {
-       getSinglePickQuestionById: jest.fn().mockReturnValue( new SinglePickQuestion(
-                   "Question ES 1", 
-                   "This is the text of a single pick question !",
-                   [ 
-                     new SingePickOption(1, 'Option 1'), 
-                     new SingePickOption(2, 'Option 2'),
-                     new SingePickOption(3, 'Option 3'),
-                     new SingePickOption(4, 'Option 4')
-                   ]))
-    };
+  it.each([0,1,2,3]) ('should select first option on first click', (firstSelect) => {
+    let mockQuestionService = returnUntouchedOptions();
     TestBed.configureTestingModule({
       imports: [SinglepickComponent],
       providers: [
@@ -59,11 +39,41 @@ describe('SinglepickComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
 
-    selectOptionById('0');
+    selectOptionById(firstSelect.toString());
 
-    expectOnlyOneOptionIsSelected(component.singlePickForm, 0);
+    expectOnlyOneOptionIsSelected(component.singlePickForm, firstSelect);
+  });
+  it.each([0,1,2,3]) ('should toggle selection with clicking twice', (firstSelect) => {
+    let mockQuestionService = returnUntouchedOptions();
+    TestBed.configureTestingModule({
+      imports: [SinglepickComponent],
+      providers: [
+        {provide: QuestionService, useValue: mockQuestionService}
+      ]
+    }).compileComponents();
+    fixture = TestBed.createComponent(SinglepickComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    selectOptionById(firstSelect.toString());
+    selectOptionById(firstSelect.toString());
+
+    expectNoOptionIsSelected(component.singlePickForm);
   });
 });
+function returnUntouchedOptions() {
+  return {
+    getSinglePickQuestionById: jest.fn().mockReturnValue(new SinglePickQuestion(
+      "Question ES 1",
+      "This is the text of a single pick question !",
+      [
+        new SingePickOption(1, 'Option 1'),
+        new SingePickOption(2, 'Option 2'),
+        new SingePickOption(3, 'Option 3'),
+        new SingePickOption(4, 'Option 4')
+      ]))
+  };
+}
 function expectOnlyOneOptionIsSelected(form: FormGroup<any>, selectedIndex: number) {
   let options = form.get('options');
   let array = options as FormArray;
@@ -75,4 +85,9 @@ function selectOptionById(id: string) {
   expect(checkBox).toBeTruthy();
   checkBox?.click();
 }
-
+function expectNoOptionIsSelected(form: FormGroup<any>) {
+  let options = form.get('options');
+  let array = options as FormArray;
+  array.controls.forEach((element, index) => { 
+    expect(element.value).toEqual(false); });
+}
